ff02df0f9d1a9654e0c98eeff160c1738efd7127
Zach Callanan <caveofmonsters@gmail.com>
2021-07-26 20:59:17 +0300

    Delay wrapperProp updating state to avoid component update while rendering error

--- app/api/ruleset-validation.ts
+++ app/api/ruleset-validation.ts
@@ -1,10 +1,12 @@
 import Cors from "cors";
+import type { NextApiRequest, NextApiResponse } from "next"
+
 
 const cors = Cors({
   methods: ["GET"],
 });
 
-function runMiddleware(req, res, fn) {
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result) => {
       if (result instanceof Error) {
@@ -15,21 +17,25 @@ function runMiddleware(req, res, fn) {
   });
 }
 
+interface Data {
+    status: string;
+}
+
 // Fetch proxy for evaluation of new ruleset link
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>): Promise<void> {
   // Add CORS headers to response
   await runMiddleware(req, res, cors);
 
   // Get data from GET headers
-  const year = req.headers["year"];
-  const version = req.headers["version"];
+  const { year, version } = req.headers;
 
   // Get data from new ruleset page
   const url = `https://media.wizards.com/${year}/downloads/MagicCompRules%${version}.txt`;
   const result = await fetch(url);
   const response = await result.text();
 
-  // API response
+  // eslint complains due to the condition, but this is is the api response
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
   response === "Not found\n"
     ? res.status(404).json({ status: "0" })
     : res.status(200).json({ status: "1" });
